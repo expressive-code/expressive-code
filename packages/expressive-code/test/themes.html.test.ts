d8a1b7d18e37cea590eb8b3985a5bf5caa8e3bce
hippotastic <6137925+hippotastic@users.noreply.github.com>
2022-09-26 12:32:27 +0200

    Start working on theme and style support

--- packages/expressive-code/test/themes.html.test.ts
+++ packages/expressive-code/test/themes.html.test.ts
@@ -1,15 +1,25 @@
 import { beforeAll, describe, expect, test } from 'vitest'
-import { addPrettierHtmlSnapshotSerializer, getAnnotationResult } from './utils'
+import { addPrettierHtmlSnapshotSerializer, createHighlighter, getAnnotationResult } from './utils'
 
 const codeSnippet = `
----
-import MyReactComponent from '../components/MyReactComponent.jsx';
-import MyAstroComponent from '../components/MyAstroComponent.astro';
----
+interface PropsType {
+  children: JSX.Element
+  name: string
+}
 
-<MyReactComponent>
-	<MyAstroComponent slot="name" />
-</MyReactComponent>
+class Component extends React.Component<PropsType, {}> {
+  render() {
+    return (
+      <h2>
+        {this.props.children}
+      </h2>
+    )
+  }
+}
+
+<Component name="foo">
+  <h1>Hello World</h1>
+</Component>
 `
 
 beforeAll(() => {
@@ -17,8 +27,14 @@ beforeAll(() => {
 })
 
 describe('Color contrast', () => {
-	test('Lightens tokens inside marked lines', () => {
+	test('Lightens tokens inside marked lines', async () => {
+		const highlighter = await createHighlighter({
+			theme: 'material-default',
+		})
+
 		const annotationResult = getAnnotationResult(codeSnippet, {
+			lang: 'tsx',
+			highlighter,
 			annotations: {
 				lineMarkings: [{ lines: [2, 7] }],
 			},
