f5ab6257d1b74d42c6a10b12d870e7c0dff54956
hippotastic <6137925+hippotastic@users.noreply.github.com>
2022-09-29 00:36:22 +0200

    Add invert test draft

--- packages/expressive-code/test/themes.test.ts
+++ packages/expressive-code/test/themes.test.ts
@@ -75,6 +75,36 @@ describe('Color contrast in marked lines', () => {
 		// Expect the (inserted) opening tag to have the same color as the (regular) closing tag
 		expect(openingTagColor.hex()).toEqual(closingTagColor.hex())
 	})
+
+	test('Inverts darker token text to lighter if contrast is too low', async () => {
+		const highlighter = await createHighlighter({
+			theme: 'min-light',
+		})
+
+		const annotationResult = getAnnotationResult(jsxCodeSnippet, {
+			lang: 'tsx',
+			highlighter,
+			annotations: {
+				lineMarkings: [{ lines: [2] }, { markerType: 'ins', lines: [10] }],
+			},
+		})
+
+		prepareHtmlSnapshot({ annotationResult, name: 'low-contrast-invert-darker-text' })
+
+		// TODO: Get background color of mark highlight
+		// TODO: Test that regular "name" text is darker than mark color
+		// TODO: Test that marked "children" text is lighter than mark color
+		// (= inverted color was picked because further darkening failed to reach min contrast)
+
+		// // Get the opening and closing `article` tags and their colors
+		// const htmlTagTokens = annotationResult.annotatedCode.shikiTokens.filter((token) => token.text === 'article')
+		// expect(htmlTagTokens).toHaveLength(2)
+		// const openingTagColor = chroma(htmlTagTokens[0].color || '')
+		// const closingTagColor = chroma(htmlTagTokens[1].color || '')
+
+		// // Expect the (inserted) opening tag to be lighter than the (regular) closing tag
+		// expect(openingTagColor.luminance()).toBeGreaterThan(closingTagColor.luminance())
+	})
 })
 
 // TODO: Add color contrast tests, including cases where color must be inverted, and where contrast cannot be improved
