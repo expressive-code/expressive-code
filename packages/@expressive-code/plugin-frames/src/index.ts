5a779c453285219135105d328c2e5bc2618ceef8
hippotastic <6137925+hippotastic@users.noreply.github.com>
2023-03-06 18:59:18 +0100

    Add code preprocessing and tests to frames plugin

--- packages/@expressive-code/plugin-frames/src/index.ts
+++ packages/@expressive-code/plugin-frames/src/index.ts
@@ -1,10 +1,19 @@
 import { ExpressiveCodePlugin, replaceDelimitedValues } from '@expressive-code/core'
 
+export interface FramesPluginOptions {
+	/**
+	 * If this is true (default) and no title was found in the code block's meta string,
+	 * the plugin will try to find and extract a comment line containing the code block file name
+	 * from the first 4 lines of the code.
+	 */
+	extractFileNameFromCode?: boolean
+}
+
 export interface FramesPluginData {
 	title?: string
 }
 
-export function frames(): ExpressiveCodePlugin {
+export function frames({ extractFileNameFromCode = true }: FramesPluginOptions = {}): ExpressiveCodePlugin {
 	return {
 		name: 'Frames',
 		hooks: {
@@ -22,6 +31,91 @@ export function frames(): ExpressiveCodePlugin {
 					return fullMatch
 				})
 			},
+			preprocessCode: ({ codeBlock, getPluginData }) => {
+				// Skip processing if the given options do not allow file name extraction from code
+				if (!extractFileNameFromCode) return
+
+				const blockData = getPluginData<FramesPluginData>('block', {})
+
+				// If we already extracted a title from the meta information,
+				// do not attempt to find a title inside the code
+				if (blockData.title) return
+
+				// Attempt to find a file name comment in the first 4 lines of the code
+				const lineIdx = codeBlock.getLines(0, 4).findIndex((line) => {
+					blockData.title = getFileNameFromComment(line.text, codeBlock.language)
+					return !!blockData.title
+				})
+
+				// Was a valid file name comment line found?
+				if (blockData.title) {
+					// Yes, remove it from the code
+					codeBlock.deleteLine(lineIdx)
+
+					// If the following line is empty, remove it as well
+					if (codeBlock.getLine(lineIdx)?.text.trim().length === 0) {
+						codeBlock.deleteLine(lineIdx)
+					}
+				}
+			},
 		},
 	}
+}
+
+const LanguageGroups = {
+	code: ['astro', 'cjs', 'htm', 'html', 'js', 'jsx', 'mjs', 'svelte', 'ts', 'tsx', 'vue'],
+	data: ['env', 'json', 'yaml', 'yml'],
+	styles: ['css', 'less', 'sass', 'scss', 'styl', 'stylus'],
+	textContent: ['markdown', 'md', 'mdx'],
+}
+
+const FileNameCommentRegExp = new RegExp(
+	[
+		// Start of line
+		`^`,
+		// Optional whitespace
+		`\\s*`,
+		// Mandatory comment start (`//`, `#` or `<!--`)
+		`(?://|#|<!--)`,
+		// Optional whitespace
+		`\\s*`,
+		// Optional sequence of characters, followed by a Japanese colon or a regular colon (`:`),
+		// but not by `://`. Matches strings like `File name:`, but not `https://example.com/test.md`.
+		`(?:(.*?)(?:\\uff1a|:(?!//)))?`,
+		// Optional whitespace
+		`\\s*`,
+		// Optional sequence of characters allowed in file paths
+		`([\\w./[\\]\\\\-]*`,
+		// Mandatory dot and supported file extension
+		`\\.(?:${Object.values(LanguageGroups).flat().sort().join('|')}))`,
+		// Optional whitespace
+		`\\s*`,
+		// Optional HTML comment end (`-->`)
+		`(?:-->)?`,
+		// Optional whitespace
+		`\\s*`,
+		// End of line
+		`$`,
+	].join('')
+)
+
+/**
+ * Checks if the given source code line is a comment that contains a file name
+ * for the code snippet.
+ *
+ * If the syntax highlighting language is contained in our known language groups,
+ * only allows file names with extensions that belong to the same language group.
+ */
+function getFileNameFromComment(line: string, lang: string) {
+	const matches = FileNameCommentRegExp.exec(line)
+	const extractedFileName = matches?.[2]
+	if (!extractedFileName) return
+
+	// Ignore the extracted file name if its extension does not belong to the same language group
+	// (e.g. JS code containing a CSS file name in a comment)
+	const languageGroup = Object.values(LanguageGroups).find((group) => group.includes(lang))
+	const fileExt = extractedFileName.match(/\.([^.]+)$/)?.[1]
+	if (languageGroup && fileExt && !languageGroup.includes(fileExt)) return
+
+	return extractedFileName
 }
---
+++ packages/@expressive-code/plugin-frames/test/preprocess-code.test.ts
@@ -0,0 +1,224 @@
+import { describe, expect, test } from 'vitest'
+import { ExpressiveCode } from '@expressive-code/core'
+import { frames, FramesPluginData, FramesPluginOptions } from '../src'
+
+describe('Extracts file name comments from the first code lines', () => {
+	test('JS comments without prefix', () => {
+		expectCodeResult({
+			code: `
+// test.config.mjs
+import { defineConfig } from 'example/config'
+				`,
+			language: 'js',
+			expected: {
+				extractedFileName: 'test.config.mjs',
+				code: `import { defineConfig } from 'example/config'`,
+			},
+		})
+	})
+
+	test('JS comments with prefix followed by a colon', () => {
+		expectCodeResult({
+			code: `
+// Example file: test.config.ts
+import { defineConfig } from 'example/config'
+				`,
+			language: 'ts',
+			expected: {
+				extractedFileName: 'test.config.ts',
+				code: `import { defineConfig } from 'example/config'`,
+			},
+		})
+	})
+
+	test('HTML comments', () => {
+		expectCodeResult({
+			code: `
+<!-- Example: src/pages/stars.htm -->
+<img src="/assets/stars.png" alt="A starry night sky.">
+				`,
+			language: 'html',
+			expected: {
+				extractedFileName: 'src/pages/stars.htm',
+				code: `<img src="/assets/stars.png" alt="A starry night sky.">`,
+			},
+		})
+	})
+
+	test('YAML comments', () => {
+		expectCodeResult({
+			code: `
+---
+# src/pages/post/blog-post.md
+layout: ../../layouts/BaseLayout.astro
+title: My Blog Post
+draft: true
+---
+
+This is my in-progress blog post.
+
+No page will be built for this post.
+				`,
+			language: 'markdown',
+			expected: {
+				extractedFileName: 'src/pages/post/blog-post.md',
+				code: `---
+layout: ../../layouts/BaseLayout.astro
+title: My Blog Post
+draft: true
+---
+
+This is my in-progress blog post.
+
+No page will be built for this post.`,
+			},
+		})
+	})
+
+	test('Removes line of whitespace afterwards', () => {
+		expectCodeResult({
+			code: `
+// test.config.mjs
+
+import { defineConfig } from 'example/config'
+				`,
+			language: 'js',
+			expected: {
+				extractedFileName: 'test.config.mjs',
+				code: `import { defineConfig } from 'example/config'`,
+			},
+		})
+	})
+})
+
+describe('Leaves comments unlikely to be file names untouched', () => {
+	test('Comments after line 4', () => {
+		const code = `
+Line 1
+Line 2
+Line 3
+Line 4
+// test.config.mjs
+
+import { defineConfig } from 'example/config'
+		`
+		expectCodeResult({
+			code,
+			language: 'js',
+			expected: {
+				extractedFileName: undefined,
+				code: code.trim(),
+			},
+		})
+	})
+
+	test('Links ending with a file name', () => {
+		const code = `
+// You can access the file like this:
+// https://example.com/test.js
+		`
+		expectCodeResult({
+			code,
+			language: 'js',
+			expected: {
+				extractedFileName: undefined,
+				code: code.trim(),
+			},
+		})
+	})
+
+	test('File extensions not matching the language', () => {
+		const code = `
+<head>
+  <!-- Local: /public/styles/global.css -->
+  <link rel="stylesheet" href="/styles/global.css" />
+  <!-- External -->
+  <link rel="stylesheet" href="https://example.com/test.css" />
+</head>
+		`
+		expectCodeResult({
+			code,
+			language: 'jsx',
+			expected: {
+				extractedFileName: undefined,
+				code: code.trim(),
+			},
+		})
+	})
+})
+
+describe('Does not extract file names from comments when disabled or not needed', () => {
+	test('When disabled in plugin configuration', () => {
+		const code = `
+// test.config.mjs
+import { defineConfig } from 'example/config'
+		`
+		expectCodeResult({
+			code,
+			language: 'js',
+			options: { extractFileNameFromCode: false },
+			expected: {
+				extractedFileName: undefined,
+				code: code.trim(),
+			},
+		})
+	})
+	test('When `title` attribute was found in meta string', () => {
+		const code = `
+// test.config.mjs
+import { defineConfig } from 'example/config'
+		`
+		expectCodeResult({
+			code,
+			language: 'js',
+			meta: 'title="something.js"',
+			expected: {
+				extractedFileName: 'something.js',
+				code: code.trim(),
+			},
+		})
+	})
+})
+
+function expectCodeResult({
+	code,
+	language,
+	meta = '',
+	options,
+	expected,
+}: {
+	code: string
+	language: string
+	meta?: string
+	options?: FramesPluginOptions
+	expected: {
+		extractedFileName: string | undefined
+		code: string
+	}
+}) {
+	// Create frames plugin
+	const plugin = frames(options)
+
+	// Wrap its code preprocessing hook into a test function that captures internal plugin data
+	const originalPreprocessMetadata = plugin.hooks.preprocessMetadata
+	let pluginData: FramesPluginData | undefined
+	plugin.hooks.preprocessMetadata = (context) => {
+		if (!originalPreprocessMetadata) return
+		originalPreprocessMetadata(context)
+		pluginData = context.getPluginData<FramesPluginData>('block', {})
+	}
+
+	// Create an Expressive Code instance with our wrapped plugin
+	// and use it to process the test code
+	const ec = new ExpressiveCode({
+		plugins: [plugin],
+	})
+	const { codeBlock } = ec.processCode({ code: code.trim(), language, meta })
+
+	const actual = {
+		extractedFileName: pluginData?.title,
+		code: codeBlock.code,
+	}
+
+	expect(actual).toMatchObject(expected)
+}
