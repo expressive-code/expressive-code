d5388ab2992985cefbd6fb06db8dfe9bac2d157b
hippotastic <6137925+hippotastic@users.noreply.github.com>
2023-03-08 11:31:44 +0100

    Implement partIndicesByAnnotationIndex, add tests

--- packages/@expressive-code/core/src/internal/rendering.ts
+++ packages/@expressive-code/core/src/internal/rendering.ts
@@ -1,14 +1,15 @@
-import { ExpressiveCodeAnnotation } from '../common/annotation'
 import { ExpressiveCodeLine } from '../common/line'
 
 export function splitLineAtAnnotationBoundaries(line: ExpressiveCodeLine) {
 	const textParts: string[] = []
-	const partIndicesByAnnotation = new Map<ExpressiveCodeAnnotation, number[]>()
+	const partIndicesByAnnotationIndex = new Map<number, number[]>()
+	const fullText = line.text
+	const annotations = line.getAnnotations()
 
 	// Create an array of unique boundaries
 	const annotationBoundaries = [
 		...new Set(
-			line.getAnnotations().flatMap(({ inlineRange }) => {
+			annotations.flatMap(({ inlineRange }) => {
 				if (!inlineRange) return []
 				return [inlineRange.columnStart, inlineRange.columnEnd]
 			})
@@ -16,7 +17,6 @@ export function splitLineAtAnnotationBoundaries(line: ExpressiveCodeLine) {
 	].sort((a, b) => a - b)
 
 	// Use the array of boundaries to split the line plaintext into parts
-	const fullText = line.text
 	let lastColumn = 0
 	annotationBoundaries.forEach((column) => {
 		if (column === lastColumn) return
@@ -25,8 +25,26 @@ export function splitLineAtAnnotationBoundaries(line: ExpressiveCodeLine) {
 	})
 	if (lastColumn < fullText.length) textParts.push(fullText.slice(lastColumn))
 
+	// Build a list of part indices contained within each annotation
+	annotations.forEach((annotation, annotationIndex) => {
+		if (!annotation.inlineRange) return
+		const { columnStart, columnEnd } = annotation.inlineRange
+		const partIndices: number[] = []
+		let partStart = 0
+		textParts.forEach((part, partIndex) => {
+			const partEnd = partStart + part.length
+			// If the part is completely contained within the current annotation's column range,
+			// add its part index to the list of part indices for the annotation
+			if (partStart >= columnStart && partEnd <= columnEnd) {
+				partIndices.push(partIndex)
+			}
+			partStart = partEnd
+		})
+		partIndicesByAnnotationIndex.set(annotationIndex, partIndices)
+	})
+
 	return {
 		textParts,
-		partIndicesByAnnotation,
+		partIndicesByAnnotationIndex,
 	}
 }
--- packages/@expressive-code/core/test/rendering.test.ts
+++ packages/@expressive-code/core/test/rendering.test.ts
@@ -9,6 +9,7 @@ describe('splitLineAtAnnotationBoundaries()', () => {
 		const line = new ExpressiveCodeLine(testText)
 		const actual = splitLineAtAnnotationBoundaries(line)
 		expect(actual.textParts).toMatchObject(['Nothing to see here!'])
+		expect([...actual.partIndicesByAnnotationIndex.entries()]).toMatchObject([])
 	})
 	test('Ignores full-line annotations', () => {
 		const line = new ExpressiveCodeLine(testText)
@@ -18,25 +19,37 @@ describe('splitLineAtAnnotationBoundaries()', () => {
 		})
 		const actual = splitLineAtAnnotationBoundaries(line)
 		expect(actual.textParts).toMatchObject(['Nothing to see here!'])
+		expect([...actual.partIndicesByAnnotationIndex.entries()]).toMatchObject([])
 	})
 	describe('Single annotation', () => {
 		test('Line starting with an annotation', () => {
 			const line = new ExpressiveCodeLine(testText)
 			annotateMatchingTextParts(line, ['Nothing'])
 			const actual = splitLineAtAnnotationBoundaries(line)
+			// Part indices:                            0              1
 			expect(actual.textParts).toMatchObject(['Nothing', ' to see here!'])
+			expect([...actual.partIndicesByAnnotationIndex.entries()]).toMatchObject([
+				// Annotation 0: 'Nothing' (part index 0)
+				[0, [0]],
+			])
 		})
 		test('Line ending with an annotation', () => {
 			const line = new ExpressiveCodeLine(testText)
 			annotateMatchingTextParts(line, ['here!'])
 			const actual = splitLineAtAnnotationBoundaries(line)
+			// Part indices:                                0             1
 			expect(actual.textParts).toMatchObject(['Nothing to see ', 'here!'])
+			expect([...actual.partIndicesByAnnotationIndex.entries()]).toMatchObject([
+				// Annotation 0: 'here!' (part index 1)
+				[0, [1]],
+			])
 		})
 		test('Annotation covering the entire text', () => {
 			const line = new ExpressiveCodeLine(testText)
 			annotateMatchingTextParts(line, [testText])
 			const actual = splitLineAtAnnotationBoundaries(line)
 			expect(actual.textParts).toMatchObject([testText])
+			expect([...actual.partIndicesByAnnotationIndex.entries()]).toMatchObject([[0, [0]]])
 		})
 	})
 	describe('Multiple non-intersecting annotations', () => {
@@ -44,13 +57,27 @@ describe('splitLineAtAnnotationBoundaries()', () => {
 			const line = new ExpressiveCodeLine(testText)
 			annotateMatchingTextParts(line, ['Nothing ', ' here!'])
 			const actual = splitLineAtAnnotationBoundaries(line)
+			// Part indices:                            0           1         2
 			expect(actual.textParts).toMatchObject(['Nothing ', 'to see', ' here!'])
+			expect([...actual.partIndicesByAnnotationIndex.entries()]).toMatchObject([
+				// Annotation 0: 'Nothing ' (part index 0)
+				[0, [0]],
+				// Annotation 1: ' here!' (part index 2)
+				[1, [2]],
+			])
 		})
 		test('Annotations touching at their boundaries', () => {
 			const line = new ExpressiveCodeLine(testText)
 			annotateMatchingTextParts(line, ['to ', 'see here'])
 			const actual = splitLineAtAnnotationBoundaries(line)
+			// Part indices:                            0         1        2        3
 			expect(actual.textParts).toMatchObject(['Nothing ', 'to ', 'see here', '!'])
+			expect([...actual.partIndicesByAnnotationIndex.entries()]).toMatchObject([
+				// Annotation 0: 'to ' (part index 1)
+				[0, [1]],
+				// Annotation 1: 'see here' (part index 2)
+				[1, [2]],
+			])
 		})
 	})
 	describe('Intersecting annotations', () => {
@@ -58,19 +85,55 @@ describe('splitLineAtAnnotationBoundaries()', () => {
 			const line = new ExpressiveCodeLine(testText)
 			annotateMatchingTextParts(line, ['see', 'see'])
 			const actual = splitLineAtAnnotationBoundaries(line)
+			// Part indices:                              0          1        2
 			expect(actual.textParts).toMatchObject(['Nothing to ', 'see', ' here!'])
+			expect([...actual.partIndicesByAnnotationIndex.entries()]).toMatchObject([
+				// Annotation 0: 'see' (part index 1)
+				[0, [1]],
+				// Annotation 1: 'see' (part index 1)
+				[1, [1]],
+			])
 		})
-		test('Two annotations where one is fully contained inside the other', () => {
+		test('Two annotations where the second is fully contained in the first', () => {
 			const line = new ExpressiveCodeLine(testText)
 			annotateMatchingTextParts(line, ['to see here', 'see'])
 			const actual = splitLineAtAnnotationBoundaries(line)
+			// Part indices:                            0         1      2        3     4
 			expect(actual.textParts).toMatchObject(['Nothing ', 'to ', 'see', ' here', '!'])
+			expect([...actual.partIndicesByAnnotationIndex.entries()]).toMatchObject([
+				// Annotation 0: 'to see here' (part indices 1, 2, 3)
+				[0, [1, 2, 3]],
+				// Annotation 1: 'see' (part index 2)
+				[1, [2]],
+			])
+		})
+		test('Two annotations where the first is fully contained in the second', () => {
+			const line = new ExpressiveCodeLine(testText)
+			annotateMatchingTextParts(line, ['see', 'to see here'])
+			const actual = splitLineAtAnnotationBoundaries(line)
+			// Part indices:                            0         1      2        3     4
+			expect(actual.textParts).toMatchObject(['Nothing ', 'to ', 'see', ' here', '!'])
+			// Ensure that partIndicesByAnnotationIndex is sorted by the annotation's index,
+			// even though the second annotation starts at a lower part index
+			expect([...actual.partIndicesByAnnotationIndex.entries()]).toMatchObject([
+				// Annotation 0: 'see' (part index 2)
+				[0, [2]],
+				// Annotation 1: 'to see here' (part indices 1, 2, 3)
+				[1, [1, 2, 3]],
+			])
 		})
 		test('Two partially intersecting annotations', () => {
 			const line = new ExpressiveCodeLine(testText)
 			annotateMatchingTextParts(line, ['to see', 'see here'])
 			const actual = splitLineAtAnnotationBoundaries(line)
+			// Part indices:                            0         1      2        3     4
 			expect(actual.textParts).toMatchObject(['Nothing ', 'to ', 'see', ' here', '!'])
+			expect([...actual.partIndicesByAnnotationIndex.entries()]).toMatchObject([
+				// Annotation 0: 'to see' (part indices 1, 2)
+				[0, [1, 2]],
+				// Annotation 1: 'see here' (part indices 2, 3)
+				[1, [2, 3]],
+			])
 		})
 	})
 	test('Everything combined', () => {
@@ -91,6 +154,25 @@ describe('splitLineAtAnnotationBoundaries()', () => {
 			'see',
 		])
 		const actual = splitLineAtAnnotationBoundaries(line)
+		// Part indices:                            0         1      2        3     4
 		expect(actual.textParts).toMatchObject(['Nothing ', 'to ', 'see', ' here', '!'])
+		expect([...actual.partIndicesByAnnotationIndex.entries()]).toMatchObject([
+			// Annotation 0: 'to ' (part index 1)
+			[0, [1]],
+			// Annotation 1: 'see here' (part indices 2, 3)
+			[1, [2, 3]],
+			// Annotation 2: 'to see here' (part indices 1, 2, 3)
+			[2, [1, 2, 3]],
+			// Annotation 3: 'see' (part index 2)
+			[3, [2]],
+			// Annotation 4: full text (part indices 0, 1, 2, 3, 4)
+			[4, [0, 1, 2, 3, 4]],
+			// Annotation 5: 'to see' (part indices 1, 2)
+			[5, [1, 2]],
+			// Annotation 6: 'see here' (part indices 2, 3)
+			[6, [2, 3]],
+			// Annotation 7: 'see' (part index 2)
+			[7, [2]],
+		])
 	})
 })
