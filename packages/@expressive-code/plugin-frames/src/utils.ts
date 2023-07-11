38500464c96b958f52548307ef4a9d36a947ad15
hippotastic <6137925+hippotastic@users.noreply.github.com>
2023-07-11 02:44:39 +0200

    Improve file type support & file name extraction

--- packages/@expressive-code/plugin-frames/src/utils.ts
+++ packages/@expressive-code/plugin-frames/src/utils.ts
@@ -21,19 +21,19 @@ export function frameTypeFromString(input: string) {
 	return frameTypes.includes(frameType) ? frameType : undefined
 }
 
-export function isTerminalLanguage(language: string) {
-	return ['shellscript', 'shell', 'bash', 'sh', 'zsh'].includes(language)
-}
-
-const LanguageGroups = {
-	code: ['astro', 'cjs', 'htm', 'html', 'js', 'jsx', 'mjs', 'svelte', 'ts', 'tsx', 'vue'],
-	terminal: ['shellscript', 'shell', 'bash', 'sh', 'zsh'],
-	data: ['env', 'json', 'yaml', 'yml'],
-	styles: ['css', 'less', 'sass', 'scss', 'styl', 'stylus'],
+export const LanguageGroups = {
+	code: ['astro', 'cjs', 'htm', 'html', 'js', 'jsx', 'mjs', 'svelte', 'ts', 'tsx', 'typescript', 'vb', 'vue', 'vue-html'],
+	terminal: ['bash', 'bat', 'batch', 'cmd', 'console', 'powershell', 'ps', 'ps1', 'psd1', 'psm1', 'sh', 'shell', 'shellscript', 'shellsession', 'zsh'],
+	data: ['csv', 'env', 'ini', 'json', 'toml', 'xml', 'yaml', 'yml'],
+	styles: ['css', 'less', 'sass', 'scss', 'styl', 'stylus', 'xsl'],
 	textContent: ['markdown', 'md', 'mdx'],
 }
 
-const FileNameCommentRegExp = new RegExp(
+export function isTerminalLanguage(language: string) {
+	return LanguageGroups.terminal.includes(language)
+}
+
+const getFileNameCommentRegExpString = () =>
 	[
 		// Start of line
 		`^`,
@@ -49,9 +49,9 @@ const FileNameCommentRegExp = new RegExp(
 		// Optional whitespace
 		`\\s*`,
 		// Optional sequence of characters allowed in file paths
-		`([\\w./[\\]\\\\-]*`,
-		// Mandatory dot and supported file extension
-		`\\.(?:${Object.values(LanguageGroups).flat().sort().join('|')}))`,
+		`([\\w./~[\\]\\\\-]*`,
+		// Optional dot and supported file extension
+		`(?:\\.(?:${Object.values(LanguageGroups).flat().sort().join('|')}))?)`,
 		// Optional whitespace
 		`\\s*`,
 		// Optional HTML comment end (`-->`)
@@ -61,7 +61,8 @@ const FileNameCommentRegExp = new RegExp(
 		// End of line
 		`$`,
 	].join('')
-)
+
+let fileNameCommentRegExp: RegExp | undefined
 
 /**
  * Checks if the given source code line is a comment that contains a file name
@@ -71,15 +72,28 @@ const FileNameCommentRegExp = new RegExp(
  * only allows file names with extensions that belong to the same language group.
  */
 export function getFileNameFromComment(line: string, lang: string) {
-	const matches = FileNameCommentRegExp.exec(line)
+	if (fileNameCommentRegExp === undefined) {
+		fileNameCommentRegExp = new RegExp(getFileNameCommentRegExpString())
+	}
+	const matches = fileNameCommentRegExp.exec(line)
 	const extractedFileName = matches?.[2]
 	if (!extractedFileName) return
 
-	// Ignore the extracted file name if its extension does not belong to the same language group
-	// (e.g. JS code containing a CSS file name in a comment)
+	// Check if the syntax highlighting language is contained in our known language groups,
+	// and determine the extension of the extracted file name (if any)
 	const languageGroup = Object.values(LanguageGroups).find((group) => group.includes(lang))
 	const fileExt = extractedFileName.match(/\.([^.]+)$/)?.[1]
-	if (languageGroup && fileExt && !languageGroup.includes(fileExt)) return
+
+	// If we're in the terminal language group, allow any file names (even without extensions)
+	// as long as they start with something that looks like the beginning of a path
+	if (languageGroup === LanguageGroups.terminal && extractedFileName.match(/^(\/|\\|\.|~)/)) {
+		return extractedFileName
+	}
+
+	// Ignore the extracted file name if it doesn't have an extension,
+	// or if its extension does not belong to the same language group
+	// (e.g. JS code containing a CSS file name in a comment)
+	if (!fileExt || (languageGroup && !languageGroup.includes(fileExt))) return
 
 	return extractedFileName
 }
