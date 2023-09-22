ef93adfef8260461393ef4347fdf4aab41e7f3ac
Hippo <6137925+hippotastic@users.noreply.github.com>
2023-09-22 19:40:30 +0200

    Improve caching logic to respect theme contents (#67)

--- packages/@expressive-code/plugin-shiki/src/cache.ts
+++ packages/@expressive-code/plugin-shiki/src/cache.ts
@@ -1,15 +1,33 @@
 import { Highlighter, Theme, getHighlighter } from 'shiki'
-import { ExpressiveCodeTheme } from '@expressive-code/core'
+import { ExpressiveCodeTheme, getStableObjectHash } from '@expressive-code/core'
 
 let highlighterPromise: Promise<Highlighter> | undefined
 
-export async function getCachedHighlighter({ theme }: { theme: ExpressiveCodeTheme }): Promise<Highlighter> {
+const themeCacheKeys = new WeakMap<ExpressiveCodeTheme, string>()
+
+export function getThemeCacheKey(theme: ExpressiveCodeTheme) {
+	let key = themeCacheKeys.get(theme)
+	if (!key) {
+		key = `${theme.name}-${getStableObjectHash({ bg: theme.bg, fg: theme.fg, settings: theme.settings })}`
+		themeCacheKeys.set(theme, key)
+	}
+	return key
+}
+
+/**
+ * Gets a cached Shiki highlighter instance for the given theme.
+ *
+ * Unfortunately, Shiki caches themes by name, so we need to ensure that the theme name changes
+ * whenever the theme contents change. This is done by using a content-dependent cache key.
+ */
+export async function getCachedHighlighter({ theme, cacheKey }: { theme: ExpressiveCodeTheme; cacheKey: string }): Promise<Highlighter> {
+	const themeUsingCacheKey = { ...theme, name: cacheKey }
 	if (highlighterPromise === undefined) {
-		highlighterPromise = getHighlighter({ theme })
+		highlighterPromise = getHighlighter({ theme: themeUsingCacheKey })
 	}
 	const highlighter = await highlighterPromise
-	if (!highlighter.getLoadedThemes().includes(theme.name as Theme)) {
-		await highlighter.loadTheme(theme)
+	if (!highlighter.getLoadedThemes().includes(cacheKey as Theme)) {
+		await highlighter.loadTheme(themeUsingCacheKey)
 	}
 	return highlighter
 }
